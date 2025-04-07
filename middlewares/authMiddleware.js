ae60b2ea272bd0424cc8201ce60aeb5b01a4f9b8
Adegbite Joshua Adeoye <adegbitejoshua07@gmail.com>
2025-04-07 13:06:05 +0100

    Resolved the errors in uploading and getting pdfs

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,25 +4,23 @@ const Admin = require('../models/Admin');
 exports.authenticateAdmin = async (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', ''); // Expecting a Bearer token
   console.log(token);
-
-  next();
   
-  // if (!token) {
-  //   return res.status(401).json({ msg: 'Access denied, no token provided' });
-  // }
+  if (!token) {
+    return res.status(401).json({ msg: 'Access denied, no token provided' });
+  }
 
-  // try {
-  //   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  //   const admin = await Admin.findById(decoded.admin.id);
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const admin = await Admin.findById(decoded.admin.id);
     
-  //   if (!admin) {
-  //     return res.status(404).json({ msg: 'Admin not found' });
-  //   }
-  //   req.admin = admin;
-  //   next();
-  // } catch (error) {
-  //   console.log(error);
+    if (!admin) {
+      return res.status(404).json({ msg: 'Admin not found' });
+    }
+    req.admin = admin;
+    next();
+  } catch (error) {
+    console.log(error);
     
-  //   res.status(400).json({ msg: 'Invalid token' });
-  // }
+    res.status(400).json({ msg: 'Invalid token' });
+  }
 };
